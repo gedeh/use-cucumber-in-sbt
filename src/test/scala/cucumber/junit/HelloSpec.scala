4427499de52ec79be73c5e11375a54941997e885
Hendra Saputra <nazklenknani@gmail.com>
2020-12-13 00:56:25 +0000

    Add simple scalatest spec

---
+++ src/test/scala/cucumber/junit/HelloSpec.scala
@@ -0,0 +1,10 @@
+package cucumber.junit
+
+import org.scalatest._
+import org.scalatest.funsuite.AnyFunSuite
+
+class HelloSpec extends AnyFunSuite {
+  test("Hello should start with H") {
+    assert("Hello".startsWith("H"))
+  }
+}
